f8e6962568a512b5ccfd09355a8f6802154bcae7
Valentin-Ioan VINTILA <vintilavalentinioan@gmail.com>
2023-04-13 10:09:26 +0300

    Periodic backup

--- net_includes.hpp
+++ net_includes.hpp
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <netinet/in.h>
+#include <netinet/tcp.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <sys/poll.h>
--- server.cpp
+++ server.cpp
@@ -19,10 +19,65 @@ void tcp_client_connected(string user_id) {
     }
 }
 
-int main() {
+int main(int argc, char *argv[]) {
+    // Disable stdout buffer
     setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-    log("sizeof(tcp_message_t) = %lu\n", sizeof(tcp_message_t));
+    int rc;
+    
+    // - - - - -
+
+    // Check the number of arguments and save them
+    if (argc != 2) {
+        log("[ server ] Server expects exactly one parameter, not %d\n", argc - 1);
+        return -1;
+    }
+    uint16_t server_port;
+    rc = sscanf(argv[2], "%hu", &server_port);
+    if (rc < 0) {
+        log("[ server ] Invalid port (NaN)\n");
+        return -1;
+    }
+    // Check if server_port is reserved (0 <= server_port <= 1023)
+    if (server_port <= 1023) {
+        log("[ server ] Invalid port number (%d < 1024)\n", server_port);
+        return -1;
+    }
+
+    // - - - - -
+
+    // UDP listen socket
+    int udp_listen_fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (udp_listen_fd < 0) {
+        log("[ server ] socket - UDP socket fail\n");
+        return -1;
+    }
+    struct sockaddr_in udp_addr;
+    memset((char *) &udp_addr, 0, sizeof(sockaddr_in));
+	udp_addr.sin_family = AF_INET;
+	udp_addr.sin_port = htons(server_port);
+	udp_addr.sin_addr.s_addr = INADDR_ANY;
+    
+    // - - - - -
+
+    // TCP listen socket
+    int tcp_listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_listen_fd < 0) {
+        log("[ server ] socket - TCP socket fail\n");
+        return -1;
+    }
+    int disable_neagle = 1;
+    rc = setsockopt(tcp_listen_fd, IPPROTO_TCP, TCP_NODELAY,
+                    &disable_neagle, sizeof(int));
+    if (rc < 0) {
+        log("[ server ] setsockopt - Could not disable neagle\n");
+        return -1;
+    }
+    struct sockaddr_in tcp_addr;
+    memset((char *) &tcp_addr, 0, sizeof(sockaddr_in));
+	tcp_addr.sin_family = AF_INET;
+	tcp_addr.sin_port = htons(server_port);
+	tcp_addr.sin_addr.s_addr = INADDR_ANY;
 
     return 0;
 }
